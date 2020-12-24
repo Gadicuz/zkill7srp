87cbcd74292a7ce57d5763404de51c51e00ea3c5
Nikolay Postnikov <nick.postnikov@gmail.com>
2020-12-24 23:03:07 +0300

    Refactor options with arrow functions

--- zkill7srp.ext/options.js
+++ zkill7srp.ext/options.js
@@ -1,4 +1,16 @@
-function save_options() {
+document.addEventListener('DOMContentLoaded', () => {
+  chrome.storage.sync.get({
+    names: '',
+    oneclick: false,
+    keeptrack: false
+  }, function(items) {
+    document.getElementById('pilots').value = items.names;
+    document.getElementById('oneclickapply').checked = items.oneclick;
+    document.getElementById('keeptrack').checked = items.keeptrack;
+  });
+});
+
+document.getElementById('save').addEventListener('click', () => {
   const names = document.getElementById('pilots').value;
   const oneclick = document.getElementById('oneclickapply').checked;
   const keeptrack = document.getElementById('keeptrack').checked;
@@ -13,26 +25,8 @@ function save_options() {
       status.textContent = '';
     }, 750);
   });
-}
+});
 
-function restore_options() {
-  chrome.storage.sync.get({
-    names: '',
-    oneclick: false,
-    keeptrack: false
-  }, function(items) {
-    document.getElementById('pilots').value = items.names;
-    document.getElementById('oneclickapply').checked = items.oneclick;
-    document.getElementById('keeptrack').checked = items.keeptrack;
-  });
-}
-
-function clear_history() {
+document.getElementById('clear').addEventListener('click', () => {
   chrome.storage.sync.remove('applied');
-}
-
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
-document.getElementById('clear').addEventListener('click',
-    clear_history);
+});
